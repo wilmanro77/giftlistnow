3318a77f130ef9097071829b5c2f131a9ea43d8b
christian david lucumi mosquera <cristian.lucumi00@usc.edu.co>
2019-08-08 05:41:45 -0500

    add middleware to /user-products route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource("/main-table", "ProductsController");
-Route::resource("/user-products", "ProductsUsersController");
+Route::resource("/user-products", "ProductsUsersController")
+	 ->middleware("auth"); //login required
