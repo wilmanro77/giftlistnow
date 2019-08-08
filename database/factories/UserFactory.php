4be9f5f94fe16ff87936b5e4a0b44f1c362b1b0c
Wilman Rojas <wrojas@wirotechnologies.com>
2019-08-08 15:17:44 -0400

    correction UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,18 +29,3 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10)];
     });
-
-$factory->define(User::class, function (Faker $faker) {
-        return [
-        'name' => "Wilman",
-        'last_name' => "Rojas",
-        'email' => "wrojas@giftlistnow.com",
-        'event_name' => "Mi Boda",
-        'start_date' => "2019-01-01 00:00:01",
-        'end_date' => "2019-12-31 23:59:59",  
-        'event_code' => "9998",
-        'email_verified_at' => now(),
-        'password' => Hash::make("12qwaszx"),
-        'remember_token' => Str::random(10)
-    ];
-});
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -21,7 +21,7 @@ class UsersTableSeeder extends Seeder
             'end_date' => "2019-12-31 23:59:59",  
             'event_code' => "9999",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make("password"),
             'remember_token' => Str::random(10)
         ]);
         
